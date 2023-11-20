6d89c42deb66daea748002d410bcd3cd0db67979
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-20 23:31:15 +0300

    Prepare for JWT auth

--- SkymeyBlazor/Model/Services/UserService.cs
+++ SkymeyBlazor/Model/Services/UserService.cs
@@ -1,10 +1,15 @@
-﻿using RestSharp;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.JSInterop;
+using RestSharp;
 using Skymey_main_lib.Models;
 using Skymey_main_lib.Models.JWT;
 using Skymey_main_lib.Models.Tables.User;
 using SkymeyBlazor.Interfaces.User;
 using System;
+using System.Net;
 using System.Net.Http.Json;
+using System.Security.Claims;
 using System.Text.Json;
 
 namespace SkymeyBlazor.Model.Services
@@ -27,6 +32,42 @@ namespace SkymeyBlazor.Model.Services
             var r = client.ExecuteAsync(request).Result.Content;
             var userd = JsonSerializer.Deserialize<AuthenticatedResponse>(r);
             return userd;
+        }
+        public async Task<AuthenticatedResponse> Refresh(AuthenticatedResponse sU_001LoginViewModel)
+        {
+            var options = new RestClientOptions("https://46.22.247.253:5007")
+            {
+                MaxTimeout = -1,
+            };
+            var client = new RestClient(options);
+            var request = new RestRequest("/api/User/Refresh", Method.Post);
+            request.AddParameter("application/json", JsonSerializer.Serialize(sU_001LoginViewModel), ParameterType.RequestBody);
+            var r = client.ExecuteAsync(request).Result.Content;
+            var userd = JsonSerializer.Deserialize<AuthenticatedResponse>(r);
+            return userd;
+        }
+
+        public async Task<List<SU_001ListViewModel>> GetUsers(string token)
+        {
+            try
+            {
+                var options = new RestClientOptions("https://46.22.247.253:5007")
+                {
+                    MaxTimeout = -1,
+                };
+                var client = new RestClient(options);
+                var request = new RestRequest("/api/User/GetUsers", Method.Get);
+                //request.AddParameter("application/json", token, ParameterType.RequestBody);
+                request.AddParameter("token", token);
+                var r = client.ExecuteAsync(request).Result.Content;
+                var userd = JsonSerializer.Deserialize<List<SU_001ListViewModel>>(r);
+                return userd;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new List<SU_001ListViewModel> { };
+            }
         }
     }
 }
