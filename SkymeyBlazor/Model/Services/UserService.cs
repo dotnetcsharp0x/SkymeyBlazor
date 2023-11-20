aff75c864435665cd3b8a2a2986dd654361e48ba
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-20 08:37:41 +0300

    add query to user service and getting tokens response

--- SkymeyBlazor/Model/Services/UserService.cs
+++ SkymeyBlazor/Model/Services/UserService.cs
@@ -1,5 +1,11 @@
-﻿using Skymey_main_lib.Models;
+﻿using RestSharp;
+using Skymey_main_lib.Models;
+using Skymey_main_lib.Models.JWT;
+using Skymey_main_lib.Models.Tables.User;
 using SkymeyBlazor.Interfaces.User;
+using System;
+using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace SkymeyBlazor.Model.Services
 {
@@ -8,6 +14,19 @@ namespace SkymeyBlazor.Model.Services
         public async Task<List<ExchangesViewModel>> GetExchanges()
         {
             return await new HttpClient().GetFromJsonAsync<List<ExchangesViewModel>>("https://46.22.247.253:5007/api/Crypto/GetExchanges");
+        }
+        public async Task<AuthenticatedResponse> Login(SU_001LoginViewModel sU_001LoginViewModel)
+        {
+            var options = new RestClientOptions("https://46.22.247.253:5007")
+            {
+                MaxTimeout = -1,
+            };
+            var client = new RestClient(options);
+            var request = new RestRequest("/api/User/Login", Method.Post);
+            request.AddParameter("application/json", JsonSerializer.Serialize(sU_001LoginViewModel), ParameterType.RequestBody);
+            var r = client.ExecuteAsync(request).Result.Content;
+            var userd = JsonSerializer.Deserialize<AuthenticatedResponse>(r);
+            return userd;
         }
     }
 }
